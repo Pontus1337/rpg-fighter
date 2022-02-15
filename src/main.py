2ea158e5fe9bb8001801a07dfb43fc7db6e132cd
Pontus1337 <lindhpontus1@gmail.com>
2022-02-15 15:12:01 +0100

    I added some stuff

--- src/main.py
+++ src/main.py
@@ -1,6 +1,31 @@
 
 import resources
-
+from resources import Character, Goblin
+from random import randint
 
 if __name__ =="__main__":
-    print("Hello world")
+    nemy = Character("Nemy", 20,5,2)
+    goblin_one = Goblin(10,3,1)
+
+    print(nemy)
+    print()
+    print(goblin_one)
+
+    fight_round = 1
+    print("============Fight=============")
+    while nemy.get_health()!=0 and goblin_one.get_health()!=0:
+        print(f"round {fight_round} ")
+        nemy_attack = nemy.damage()*randint(1,3)
+        goblin_one.take_damage(nemy_attack)
+        if (goblin_one.get_health()==0):
+            print("goblin has died.")
+            break
+        else:
+            print(f" goblin has {goblin_one.get_health()}hp left.")
+            goblin_attack=goblin_one.damage()*randint(1,3)
+            nemy.take_damage(goblin_attack)
+            print(f"Nemy has {nemy.get_health()}hp left")
+            if (nemy.get_health()==0):print("nemy has died.")
+        fight_round+=1
+    if(nemy.get_health()==0): print("the goblin won!")
+    else:print("nemy has won")
--- src/resources.py
+++ src/resources.py
@@ -12,6 +12,18 @@ class Character:
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
 
+    def damage(self):
+        return self.attack
+
+    def take_damage(self, damage):
+        relative_damage= damage-self.armor
+        if relative_damage>0:
+            self.health-=relative_damage
+        if self.health <0 :self.health=0
+
+    def get_health(self):
+        return self.health
+
 class Goblin:
 
     def __init__(self,  health, attack, armor):
@@ -20,6 +32,17 @@ class Goblin:
         self.armor=armor
 
     def __str__(self) -> str:
-        return f"Goblin: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+        return f"Goblin:\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
 
-print("test")
+    def damage(self):
+        return self.attack
+
+    def take_damage(self, damage):
+        relative_damage= damage-self.armor
+        if relative_damage>0:
+            self.health-=relative_damage
+        if self.health <0 :self.health=0
+
+    def get_health(self):
+        return self.health
+
